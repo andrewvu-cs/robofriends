bfbbf726ff545260ea2c36a0ccdc8c940acb2260
Andrew <andrewvu.cs@gmail.com>
2020-02-04 14:34:55 -0800

    removed robots.js, calls placeholder json

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import { robotsList } from "./robots";
-import './App.css'
+import "./App.css";
 import "tachyons";
+import Axios from "axios";
 
 function App() {
-  const [robots, setRobots] = useState(robotsList);
+  const [robots, setRobots] = useState([]);
   const [searchfield, setSearchfield] = useState("");
 
+  useEffect(() => {
+    Axios.get("https://jsonplaceholder.typicode.com/users").then(res => {
+      setRobots(res.data);
+    });
+  }, []);
+
   const onSearchChange = event => {
-        setSearchfield(event.target.value);
+    setSearchfield(event.target.value);
   };
 
   const filteredRobots = robots.filter(robot => {
@@ -19,7 +26,7 @@ function App() {
 
   return (
     <div className="tc">
-      <h1 className='f2'>RoboFriends</h1>
+      <h1 className="f2">RoboFriends</h1>
       <SearchBox searchChange={onSearchChange} />
       <CardList robots={filteredRobots} />
     </div>
