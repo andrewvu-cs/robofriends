383844d0a2ae1901be17f745581d90743d248674
Andrew <andrewvu.cs@gmail.com>
2020-02-11 14:30:14 -0800

    added redux middleware and split up reducers

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,23 @@
-import {CHANGE_SEARCH_FIELD} from './constants';
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED
+} from "./constants";
+import Axios from "axios";
 
 // payload sending whatever data is needed
-export const setSearchField = (text) => ({
+export const setSearchField = text => ({
   type: CHANGE_SEARCH_FIELD,
-  payload: text  
-})
+  payload: text
+});
+
+export const requestRobots = () => (dispatch) => {
+  dispatch({ type: REQUEST_ROBOTS_PENDING});
+  Axios.get("https://jsonplaceholder.typicode.com/users")
+    .then(res => dispatch({type: REQUEST_ROBOTS_SUCCESS, payload: res.data}))
+    .catch(error => dispatch({type: REQUEST_ROBOTS_FAILED, payload: error}))
+    ;
+};
+
+
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,41 +1,42 @@
 import React, { useState, useEffect, useCallback } from "react";
-import {useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import "./App.css";
 import "tachyons";
-import Axios from "axios";
-import { setSearchField } from "../actions";
-
+import { setSearchField, requestRobots } from "../actions";
 
 // tell me whhat props i should listen to for actions to get dispatched
 
-
-function App({props}) {
-  const [robots, setRobots] = useState([]);
+function App({ props }) {
+  // const [robots, setRobots] = useState([]);
   // const {searchField, onSearchChange} = this.props;
 
-  const searchField = useSelector(state => state.searchField)
+  const searchField = useSelector(state => state.searchRobots.searchField);
+  const robots = useSelector(state => state.requestRobots.robots);
+  const isPending = useSelector(state => state.requestRobots.isPending);
+  const error = useSelector(state => state.requestRobots.error);
   const dispatch = useDispatch();
 
   const updateSearchField = useCallback(
-    (event) => dispatch(setSearchField(event.target.value)),
+    event => dispatch(setSearchField(event.target.value)),
     [dispatch]
-  )
+  );
 
+  const onRequestRobots = useCallback(() => dispatch(requestRobots()), [dispatch]);
+  
   useEffect(() => {
-    Axios.get("https://jsonplaceholder.typicode.com/users").then(res => {
-      console.log(res.data);
-      setRobots(res.data);
-    });
-  }, []);
+    onRequestRobots();
+  },[onRequestRobots])
 
   const filteredRobots = robots.filter(robot => {
     return robot.name.toLowerCase().includes(searchField.toLowerCase());
   });
 
-  return (
+  return isPending ? (
+    <h1>Loading</h1>
+  ) : (
     <div className="tc">
       <h1 className="f2">RoboFriends</h1>
       <SearchBox searchChange={updateSearchField} />
@@ -51,4 +52,4 @@ function App({props}) {
   );
 }
 
-export default (App);
+export default App;
