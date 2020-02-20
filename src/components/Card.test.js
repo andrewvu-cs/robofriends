38db5b7d7ec8c68373a3f261a2bcc6257e3cf046
Andrew <andrewvu.cs@gmail.com>
2020-02-20 10:11:57 -0800

    created snapshot testing with enzyme-to-json

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,6 +1,7 @@
-import { shallow, mount, render } from "enzyme";
+import { shallow } from "enzyme";
 import React from "react";
 import Card from "./Card";
+import toJson from 'enzyme-to-json'
 
 // Mainly using shallow 99% of the time
 // mount does full DOM rendering components that interact with the dom, might take too much time use JSDom
@@ -9,6 +10,9 @@ import Card from "./Card";
 // shallow rendering is useful because it just renders the component
 // One Component at a time/ Unit testing
 // Does not assert on the child components
+
+// Taking a snapshot is like caching and makes testing faster
 it("expect to render Card component", () => {
-  expect(shallow(<Card />).length).toEqual(1);
+const wrapper = shallow(<Card/>)
+  expect(toJson(wrapper)).toMatchSnapshot();
 });
